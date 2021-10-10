2cfb7333a310b2e5a723cad9c966f7548b2f7961
hryak16 <91027347+hryak16@users.noreply.github.com>
2021-10-11 00:52:53 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> PHP Program </title>
+</head>
+<body>
+<?php
+    $name = "Люба";
+    $age = 18;
+
+    echo "Меня зовут $name <br>";
+    print "Мне $age лет"
+?>
+</body>
+</html>
